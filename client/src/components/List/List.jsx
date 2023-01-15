375fc6bbcc16a8fca737a5ee4bb5faa25ed9a230
Brandon <brandon93.w@googlemail.com>
2023-01-15 15:14:55 +0100

    filter data

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,53 +1,19 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 import Card from '../Card/Card'
 import './List.scss'
 
-const List = () => {
+const List = ({ subCats, maxPrice, sort, catId }) => {
 
-    const data = [
-        {
-            id: 1,
-            img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            img2: "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            title: "Long Sleeve",
-            isNew: true,
-            oldPrice: 19,
-            price: 12
-
-        },
-        {
-            id: 2, 
-            img: "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            title: "Coat",
-            isNew: true,
-            oldPrice: 19,
-            price: 12
-            
-        },
-        {
-            id: 3,
-            img: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            title: "Jeans",
-            isNew: false,
-            oldPrice: 20,
-            price: 13
-            
-        },
-        {
-            id: 4,
-            img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            title: "Shirt",
-            isNew: false,
-            oldPrice: 9,
-            price: 5
-            
-        }
-    ]
+    const {data, loading, error} = useFetch(`/products?populate=*&[filters][categories][id]=${catId}${subCats.map(item => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+    )
 
   return (
-    <div className='list'>{data?.map(item =>(
-        <Card item={item} key={item.key} />
-    ))}</div>
+    <div className='list'>
+        {loading ? "loading" : data?.map((item) =>
+        <Card item={item} key={item.key} /> )}
+    </div>
   )
 }
 
